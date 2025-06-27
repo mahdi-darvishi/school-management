c457f11bdcee8dbb1928032aa19405bd53ed3203
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-06-27 12:40:13 +0330

    refactor: update LessonListPage to import role from utils and enhance actions rendering based on user role

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -2,9 +2,9 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/setting";
+import { role } from "@/lib/utils";
 import { Class, Lesson, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 
@@ -28,10 +28,14 @@ const columns = [
     accessor: "teacher",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Actions",
-    accessor: "action",
-  },
+  ...(role === "admin" || role === "teacher"
+    ? [
+        {
+          header: "Actions",
+          accessor: "actions",
+        },
+      ]
+    : []),
 ];
 
 const renderRow = (item: LessonList) => (
