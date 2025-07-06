6d559de8432963bc12c68e139560da83c3271a8f
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-07-06 18:46:30 +0330

    refactor: simplify user display in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 
 const Navbar = async () => {
   const user = await currentUser();
-
   return (
     <div className="flex items-center justify-between p-4">
       {/* SEARCH BAR */}
@@ -28,9 +27,7 @@ const Navbar = async () => {
           </div>
         </div>
         <div className="flex flex-col">
-          <span className="text-xs leading-3 font-medium">
-            {user?.username}
-          </span>
+          <span className="text-xs leading-3 font-medium">John Doe</span>
           <span className="text-[10px] text-gray-500 text-right">
             {user?.publicMetadata?.role as string}
           </span>
